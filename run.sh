421b6a7230e404f69963567282e0445f8dff3269
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-10-06 17:47:47 -0500

    Trying some new cmds to install python 2.7 on the VM

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# install python 2.7
+sudo yum update
+sudo yum install scl-utils
+sudo yum install centos-release-scl-rh
+sudo yum install python27
+sudo scl enable python27 bash
+export PYTHONPATH=/usr/lib/python2.7/site-packages/:$PYTHONPATH
+
+# sudo yum makecache
+# sudo yum install yum-utils
+# sudo yum install https://centos7.iuscommunity.org/ius-release.rpm
+# sudo yum makecache
+# sudo yum install python36u
+
+# sudo yum install python-pip
+# sudo pip install starbase
+
 # run hbase commands
-cat src/hbase_cmds.txt | hbase shell -n
+# cat src/hbase_cmds.txt | hbase shell -n
 

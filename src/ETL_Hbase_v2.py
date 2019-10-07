15e9e9187f02d886f27f7400cd2650f86767f0bf
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-10-06 22:29:08 -0500

    Falling back to table insert of batch insert fails

--- src/ETL_Hbase_v2.py
+++ src/ETL_Hbase_v2.py
@@ -46,31 +46,40 @@ class HBase(Connection):
       table.create(*col_names)
       return table
       
-      
-def PopulateTable(reviews, table):
-   """
-      BRIEF  Copy all the date from the file to the table
-   """
-   batch = table.batch()
-   for review in reviews:
-      batch.insert(
+   @staticmethod
+   def PopulateTable(reviews, table):
+      """
+         BRIEF  Copy all the date from the file to the table
+      """
+      batch = table.batch()
+      if batch:
+         HBase._InsertReviews(reviews, batch)
+         batch.commit(finalize = True)
+      else:
+         HBase._InsertReviews(reviews, table)
          
-         # unique row key (assuming user can't review same movie twice!)
-         review[Review.USER_ID] + review[Review.MOVIE_ID],
+   @staticmethod
+   def _InsertReviews(reviews, table):
+      """
+      """
+      for review in reviews:
+         table.insert(
+            
+            # unique row key (assuming user can't review same movie twice!)
+            review[Review.USER_ID] + review[Review.MOVIE_ID],
+            
+            # columns
+            {
+               { "{0}.{1}".format(ColFamily.USER, Col.USER_NAME) : review[Review.USER_NAME]},
+               { "{0}.{1}".format(ColFamily.PROD, Col.HELPFUL  ) : review[Review.HELPFUL  ]},
+               { "{0}.{1}".format(ColFamily.PROD, Col.SCORE    ) : review[Review.SCORE    ]},
+               { "{0}.{1}".format(ColFamily.PROD, Col.TIME     ) : review[Review.TIME     ]},
+               { "{0}.{1}".format(ColFamily.PROD, Col.SUMMARY  ) : review[Review.SUMMARY  ]},
+               { "{0}.{1}".format(ColFamily.PROD, Col.TEXT     ) : review[Review.TEXT     ]}
+            }
+         )
          
-         # Column families
-         {
-            { "{0}.{1}".format(ColFamily.USER, Col.USER_NAME) : review[Review.USER_NAME]},
-            { "{0}.{1}".format(ColFamily.PROD, Col.HELPFUL  ) : review[Review.HELPFUL  ]},
-            { "{0}.{1}".format(ColFamily.PROD, Col.SCORE    ) : review[Review.SCORE    ]},
-            { "{0}.{1}".format(ColFamily.PROD, Col.TIME     ) : review[Review.TIME     ]},
-            { "{0}.{1}".format(ColFamily.PROD, Col.SUMMARY  ) : review[Review.SUMMARY  ]},
-            { "{0}.{1}".format(ColFamily.PROD, Col.TEXT     ) : review[Review.TEXT     ]}
-         }
-      )
-   batch.commit(finalize = True)
-   
-   
+         
 if __name__ == '__main__':
    
    import os
@@ -83,6 +92,6 @@ if __name__ == '__main__':
    
    # Batch insert reviews
    path = os.path.join('..', 'test', 'movies.txt')
-   Parse(path, 10**5, PopulateTable, table)
+   Parse(path, 10**5, HBase.PopulateTable, table)
    
    

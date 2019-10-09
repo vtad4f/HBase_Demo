f58b2df35831b315bfa36841ac0f0e6e5704f438
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-10-08 21:00:23 -0500

    More thorough test for all the valid ports

--- test/test.py
+++ test/test.py
@@ -18,66 +18,71 @@ HBASE_PORTS = set(map(str, [ # cloudera 5.14.x & 5.14.x
    11060, # hbase-solr-indexer
 ]))
 
+valid_ports = []
 for port in HBASE_PORTS:
    try:
       c = Connection(port = port)
       t = c.table('tbl')
       t.create('col')
       t.drop()
+      valid_ports.append(port)
       print("Connection success! port {0}".format(port))
    except ConnectionError:
       print("Connection failed!  port {0}".format(port))
    sys.stdout.flush()
    
-c = Connection(port = "8085")
-t = c.table('table3')
+for port in valid_ports:
+   print("{0}\nport={1}\n{0}".format('-'*50, port))
 
-print('t.create', t.create('column1', 'column2', 'column3'))
-print('t.exists', t.exists())
-print('t.add_columns', t.add_columns('column4', 'column5', 'column6', 'column7'))
-print('t.drop_columns', t.drop_columns('column6', 'column7'))
-print('t.columns', t.columns())
-print('t.drop', t.drop())
+   c = Connection(port = port)
+   t = c.table('table3')
 
-print('t.insert', t.insert(
-   'my-key-1',
-   {
-      'column1': {'key11': 'value 11', 'key12': 'value 12', 'key13': 'value 13'},
-      'column2': {'key21': 'value 21', 'key22': 'value 22'},
-      'column3': {'key32': 'value 31', 'key32': 'value 32'}
-   }
-))
-print('t.fetch', t.fetch('my-key-1'))
-print('t.fetch', t.fetch('my-key-1', ['column1', 'column2']))
-print('t.fetch', t.fetch('my-key-1', {'column1': ['key11', 'key13'], 'column3': ['key32']}))
-print('t.fetch', t.fetch('my-key-1', ['column1:key11', 'column1:key13', 'column3:key32']))
-print('t.fetch', t.fetch('my-key-1', ['column1:key11', 'column1:key13', 'column3:key32'], perfect_dict=False))
+   print('t.create', t.create('column1', 'column2', 'column3'))
+   print('t.exists', t.exists())
+   print('t.add_columns', t.add_columns('column4', 'column5', 'column6', 'column7'))
+   print('t.drop_columns', t.drop_columns('column6', 'column7'))
+   print('t.columns', t.columns())
+   print('t.drop', t.drop())
 
-
-print('t.insert', t.insert(
-   'my-key-1a',
-   {
-      'column1:key11': 'value 11', 'column1:key12': 'value 12', 'column1:key13': 'value 13',
-      'column2:key21': 'value 21', 'column2:key22': 'value 22',
-      'column3:key32': 'value 31', 'column3:key32': 'value 32'
-   }
-))
-print('t.fetch', t.fetch('my-key-1a'))
+   print('t.insert', t.insert(
+      'my-key-1',
+      {
+         'column1': {'key11': 'value 11', 'key12': 'value 12', 'key13': 'value 13'},
+         'column2': {'key21': 'value 21', 'key22': 'value 22'},
+         'column3': {'key32': 'value 31', 'key32': 'value 32'}
+      }
+   ))
+   print('t.fetch', t.fetch('my-key-1'))
+   print('t.fetch', t.fetch('my-key-1', ['column1', 'column2']))
+   print('t.fetch', t.fetch('my-key-1', {'column1': ['key11', 'key13'], 'column3': ['key32']}))
+   print('t.fetch', t.fetch('my-key-1', ['column1:key11', 'column1:key13', 'column3:key32']))
+   print('t.fetch', t.fetch('my-key-1', ['column1:key11', 'column1:key13', 'column3:key32'], perfect_dict=False))
 
 
-print('t.update', t.update(
-   'my-key-1',
-   {
-      'column4': {'key41': 'value 41', 'key42': 'value 42'}
-   }
-))
-print('t.fetch', t.fetch('my-key-1'))
+   print('t.insert', t.insert(
+      'my-key-1a',
+      {
+         'column1:key11': 'value 11', 'column1:key12': 'value 12', 'column1:key13': 'value 13',
+         'column2:key21': 'value 21', 'column2:key22': 'value 22',
+         'column3:key32': 'value 31', 'column3:key32': 'value 32'
+      }
+   ))
+   print('t.fetch', t.fetch('my-key-1a'))
 
 
-print('t.remove', t.remove('my-key-1', 'column4'))
-print('t.fetch', t.fetch('my-key-1'))
+   print('t.update', t.update(
+      'my-key-1',
+      {
+         'column4': {'key41': 'value 41', 'key42': 'value 42'}
+      }
+   ))
+   print('t.fetch', t.fetch('my-key-1'))
 
 
-print('t.remove', t.remove('my-key-1'))
-print('t.fetch', t.fetch('my-key-1'))
+   print('t.remove', t.remove('my-key-1', 'column4'))
+   print('t.fetch', t.fetch('my-key-1'))
+
+
+   print('t.remove', t.remove('my-key-1'))
+   print('t.fetch', t.fetch('my-key-1'))
 
